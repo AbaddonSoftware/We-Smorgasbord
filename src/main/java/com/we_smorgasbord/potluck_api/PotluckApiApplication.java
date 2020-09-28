2ec357e0ba6ca187d1c46bb1fb93a02f80596d21
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-09-27 23:40:37 -0400

    Created Springboot Skeleton for API

---
+++ src/main/java/com/we_smorgasbord/potluck_api/PotluckApiApplication.java
@@ -0,0 +1,13 @@
+package com.we_smorgasbord.potluck_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotluckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotluckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/we_smorgasbord/potluck_api/PotluckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.we_smorgasbord.potluck_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotluckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
