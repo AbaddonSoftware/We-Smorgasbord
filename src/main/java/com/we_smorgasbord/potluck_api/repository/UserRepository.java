6e9776f52bc4bb09b575ee2f1a00945b8c7963bd
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-09-29 00:25:18 -0400

    Added UserRepository

---
+++ src/main/java/com/we_smorgasbord/potluck_api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.we_smorgasbord.potluck_api.repository;
+
+import com.we_smorgasbord.potluck_api.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
