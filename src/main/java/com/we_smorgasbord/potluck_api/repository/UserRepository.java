4cfd6968f5d8b070b276a645be0aad5354a81940
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-09-29 00:35:33 -0400

    Keep coding maybe there will be a breakthrough

--- src/main/java/com/we_smorgasbord/potluck_api/repository/UserRepository.java
+++ src/main/java/com/we_smorgasbord/potluck_api/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.we_smorgasbord.potluck_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    User findByName(String Name);  
 }
