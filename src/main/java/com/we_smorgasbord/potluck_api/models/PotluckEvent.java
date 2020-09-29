3a49db01eba52ee13c40005af475ee64354e771b
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-09-29 00:20:31 -0400

    Added Skeleton for PotluckEvent class

--- src/main/java/com/we_smorgasbord/potluck_api/models/PotluckEvent.java
+++ src/main/java/com/we_smorgasbord/potluck_api/models/PotluckEvent.java
@@ -1,7 +1,39 @@
 package com.we_smorgasbord.potluck_api.models;
 
+import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="potluck")
 public class PotluckEvent {
-    
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Instant creationDate;
+
+    private String name;
+
+    // TODO: Implement a better way of handling event date
+    private String EventDate;
+
+    // TODO: Customize Location implementation
+    private String EventLocation;
+
+    private String description;
+
+    private String imageLocation;
+    
 }
